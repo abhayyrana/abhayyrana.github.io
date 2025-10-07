0a06ac7cb49a7e667650afea493a77209e06db24
abhayyrana <abhayymayy@gmail.com>
2025-10-07 15:20:16 +0530

    back to sq1.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/abhayyrana/", // <-- same as your repo name
+  base: "/abhayfolio/", // <-- same as your repo name
 });
